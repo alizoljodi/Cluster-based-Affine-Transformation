f2c7e793e4576dfb1d873282aab3ec3cfe4abe10
alizoljodi <ali.zoljodi@gmail.com>
2025-09-03 11:20:21 +0200

    sw

--- main_imagenet.py
+++ main_imagenet.py
@@ -265,7 +265,7 @@ if __name__ == '__main__':
 
     # Extract logits from quantized and full-precision models using a seeded random subset of training data
     extractor = get_logits(q_model=qnn, fp_model=fp_model, dataloader=train_loader, device=device,
-                           num_samples=10, seed=args.seed)
+                           num_samples=1024, seed=args.seed)
     print('[Main] Extracting logits from models...')
     all_q, all_fp = extractor()
     print(f"[Main] Logits extracted. all_q shape: {tuple(all_q.shape)}, all_fp shape: {tuple(all_fp.shape)}")
