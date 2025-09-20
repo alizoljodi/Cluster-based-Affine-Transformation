7469461cd3e8447db4dbdee5a9d6a510078c0666
Ali Zoljodi <ali.zoljodi@mdu.se>
2025-09-20 13:01:45 +0200

    init

--- ablate_init/run_r18_w2a4_minmax_minmax_scale.sh
+++ ablate_init/run_r18_w2a4_minmax_minmax_scale.sh
@@ -11,6 +11,6 @@
 
 source /home/alz07xz/project/PD-Quant/pd_quant/bin/activate
 echo "Starting ResNet18 W2A4 ablation (init_wmode=minmax, init_amode=minmax_scale) at $(date)"
-python run_script_seed.py resnet18 --w_bits 2 --a_bits 4 --num_seeds 10 --start_seed 0 --sleep 0.5 --alpha 0.5 --num_clusters 56 --pca_dim 40 --init_wmode minmax --init_amode minmax_scale --use_enhanced_cat False
+python ../run_script_seed.py resnet18 --w_bits 2 --a_bits 4 --num_seeds 10 --start_seed 0 --sleep 0.5 --alpha 0.5 --num_clusters 56 --pca_dim 40 --init_wmode minmax --init_amode minmax_scale --use_enhanced_cat False
 echo "Completed ResNet18 W2A4 ablation (init_wmode=minmax, init_amode=minmax_scale) at $(date)"
 
